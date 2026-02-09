eba06f5c1998598d9e8ce7f347419066de13b0e7
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2026-02-09 19:48:15 +0700

    Get product from DB then print it to view

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.product = (req, res) => {
+const Product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.product = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+  console.log(products);
   res.render("admin/pages/products/index", {
     pageTitle: "Trang sản phẩm",
+    products: products,
   });
 };
