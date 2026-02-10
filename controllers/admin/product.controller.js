1a2dd1a90bfcaf78a49401e51cb6fe63cd3fea3c
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2026-02-10 15:48:53 +0700

    filter optimization

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,38 +1,13 @@
 const Product = require("../../models/product.model");
-
+const filterStatusHelper = require("../../helpers/filterStatus");
 // [GET] /admin/products
 module.exports.product = async (req, res) => {
+  const filterStatus = filterStatusHelper(req.query);
+  // console.log(filterStatus);
+
   let find = {
     deleted: false,
   };
-
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: "",
-    },
-  ];
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status,
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
 
   if (req.query.status) {
     find.status = req.query.status;
