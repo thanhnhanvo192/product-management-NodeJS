4bb645b7dd98d1d9578332174d7f26ecc449795d
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2026-02-10 14:12:47 +0700

    status filtering feature

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,44 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products
 module.exports.product = async (req, res) => {
-  const products = await Product.find({
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status,
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+  let find = {
     deleted: false,
-  });
-  console.log(products);
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  const products = await Product.find(find);
+
   res.render("admin/pages/products/index", {
     pageTitle: "Trang sản phẩm",
     products: products,
+    filterStatus: filterStatus,
   });
 };
