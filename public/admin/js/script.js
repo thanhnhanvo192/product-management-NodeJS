e69a4310d6bf6f4171ae1c1a00aaa172d0b181e8
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2026-03-15 22:01:33 +0700

    image preview

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -141,3 +141,21 @@ if (alertShow) {
   });
 }
 // End Show alert
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector(
+    "[upload-image-preview]",
+  );
+
+  uploadImageInput.addEventListener("change", (e) => {
+    console.log(e);
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Image
